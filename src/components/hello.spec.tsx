442e05b4a8cdf1aedc67d49dbdfa2f3b392e9f46
somto96 <motivatedsomto@gmail.com>
2023-01-04 18:03:08 +0100

    Configured eslint

--- src/components/hello.spec.tsx
+++ src/components/hello.spec.tsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Hello from "./Hello";
 
 it("renders 'Hello World!'", () => {
-  render(<Hello/>);
+  render(<Hello />);
+  const myElement = screen.getByText(/Hello World/);
+  expect(myElement).toBeInTheDocument();
 });
